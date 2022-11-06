36cfc3c308343b435e85370a85479d7ebf479598
Brianna Stusiak <bstusiak@gmail.com>
2022-11-05 18:03:29 -0700

    Added print statement

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -16,6 +16,7 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("New print statement!");
     }
     
 }
